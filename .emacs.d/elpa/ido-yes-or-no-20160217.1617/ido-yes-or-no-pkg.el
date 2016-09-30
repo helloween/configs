6b5df33d34c119d91d7327f339c522d176ca9d3c
Yury Gurski <hi1sailor@gmail.com>
2016-09-30 14:09:33 +0300

    additional packages

---
+++ .emacs.d/elpa/ido-yes-or-no-20160217.1617/ido-yes-or-no-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ido-yes-or-no" "20160217.1617" "Use Ido to answer yes-or-no questions" '((ido-completing-read+ "0")) :url "https://github.com/DarwinAwardWinner/ido-yes-or-no")
